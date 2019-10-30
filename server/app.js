625ec294e66707a1c74e441e44843e11686f6599
Lluis Cifre <luis.cifre@ejuniper.com>
2019-10-30 17:27:29 +0100

    Show socket info by sockets

--- server/app.js
+++ server/app.js
@@ -15,8 +15,26 @@ app.use(express.static(path.resolve('client')));
 server.listen(process.env.SERVER_PORT);
 console.log('Server started on port: ' + process.env.SERVER_PORT);
 
+var SOCKET_LIST = [];
 // Socket.io config
+
 var io = require('socket.io')(server, {});
 io.sockets.on('connection', function(socket) {
-  console.log(socket.id);
-})
+  socket.x = 0;
+  socket.y = 0;
+  SOCKET_LIST.push(socket);
+});
+
+setInterval(function() {
+  var pack = [];
+  SOCKET_LIST.forEach((socket) => {
+    socket.x++;
+    socket.y++;
+    pack.push({
+      y: socket.y,
+      x: socket.x
+    });
+
+    socket.emit('newPositions', pack);
+  });
+}, 1000/25);
