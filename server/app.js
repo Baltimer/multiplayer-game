2ce7d32f12dbb5f2a4dadd33c3c920804e5e4fa0
Lluis Cifre <luis.cifre@ejuniper.com>
2019-10-31 12:47:50 +0100

    Added concurrence and movement

--- server/app.js
+++ server/app.js
@@ -16,25 +16,68 @@ server.listen(process.env.SERVER_PORT);
 console.log('Server started on port: ' + process.env.SERVER_PORT);
 
 var SOCKET_LIST = [];
+var PLAYER_LIST = [];
+
+var Player = function(id) {
+  var self = {
+    x: 250,
+    y: 250,
+    id: id,
+    number: "" + Math.floor(10 * Math.random()),
+    pressingRight: false,
+    pressingLeft: false,
+    pressingUp: false,
+    pressingDown: false,
+    maxSpeed: 10
+  }
+
+  self.updatePosition = function() {
+    if(self.pressingRight) self.x += self.maxSpeed;
+    if(self.pressingLeft) self.x -= self.maxSpeed;
+    if(self.pressingUp) self.y -= self.maxSpeed;
+    if(self.pressingDown) self.y += self.maxSpeed;
+  }
+
+  return self;
+}
 // Socket.io config
 
 var io = require('socket.io')(server, {});
 io.sockets.on('connection', function(socket) {
-  socket.x = 0;
-  socket.y = 0;
   SOCKET_LIST.push(socket);
+
+  var player = Player(socket.id);
+  PLAYER_LIST.push(player);
+
+  socket.on('disconnect', function() {
+    SOCKET_LIST = SOCKET_LIST.filter((sock) => sock.id != socket.id)
+    PLAYER_LIST = PLAYER_LIST.filter((play) => play.id != socket.id)
+  });
+
+  socket.on('keyPress', function(data) {
+    if (data.inputId === 'left')
+      player.pressingLeft = data.state;
+    else if(data.inputId === 'right')
+      player.pressingRight = data.state;
+    else if(data.inputId === 'up')
+      player.pressingUp = data.state;
+    else if(data.inputId === 'down')
+      player.pressingDown = data.state;
+  });
 });
 
 setInterval(function() {
   var pack = [];
-  SOCKET_LIST.forEach((socket) => {
-    socket.x++;
-    socket.y++;
+  PLAYER_LIST.forEach((player) => {
+    player.updatePosition();
     pack.push({
-      y: socket.y,
-      x: socket.x
+      y: player.y,
+      x: player.x,
+      number: player.number
     });
+  });
 
+  SOCKET_LIST.forEach((socket) => {
     socket.emit('newPositions', pack);
   });
 }, 1000/25);
