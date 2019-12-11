54fbf286ebedbc8faf30e4fb0caf0f629bc0ab8f
Lluis Cifre <luis.cifre@ejuniper.com>
2019-12-11 17:04:05 +0100

    Added images to render (Error on render)

--- server/app.js
+++ server/app.js
@@ -13,7 +13,8 @@ var path = require('path');
 app.get('/', function(req, res) {
   res.sendFile(path.resolve('client/index.html'));
 });
-app.use(express.static(path.resolve('client')));
+// app.use(express.static(path.resolve('client')));
+app.use('/client', express.static(__dirname + '/client'));
 
 server.listen(process.env.SERVER_PORT);
 console.log('Server started on port: ' + process.env.SERVER_PORT);
